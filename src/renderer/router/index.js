a61dbf3f879967a54626e050dd7373277a1a8d6d
XuweiyiChen <62165079+XuweiyiChen@users.noreply.github.com>
2020-12-31 00:30:32 +0800

    update for ugly ui

--- src/renderer/router/index.js
+++ src/renderer/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import vocabularyList from '../view/vocabulary'
 
 Vue.use(Router)
 
@@ -7,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'landing-page',
-      component: require('@/components/LandingPage').default
+      name: 'vocabularyList',
+      component: vocabularyList
     },
     {
       path: '*',
